34d47547421b365a1e7de5a596064f211472f516
Nicolas <nicobou60@gmail.com>
2025-12-01 12:42:57 +0100

    Initial commit: setup React + Vite + TS

---
+++ recrutplus-backend/src/main/java/com/ensitech/recrutplus/RecrutplusApplication.java
@@ -0,0 +1,13 @@
+package com.ensitech.recrutplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutplusApplication.class, args);
+	}
+
+}
