4339f6b6b67c67bb5c78113f3e301ca515c0f7be
Nicolas <nicobou60@gmail.com>
2026-04-29 00:20:14 +0200

    fix: restore complete EmailService from dev

--- recrutplus-backend/src/main/java/com/recrutplus/service/impl/EmailService.java
+++ recrutplus-backend/src/main/java/com/recrutplus/service/impl/EmailService.java
@@ -1,14 +1,15 @@
 package com.recrutplus.service.impl;
 
-import com.recrutplus.model.enums.ApplicationStatus;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import com.recrutplus.model.enums.ApplicationStatus;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
