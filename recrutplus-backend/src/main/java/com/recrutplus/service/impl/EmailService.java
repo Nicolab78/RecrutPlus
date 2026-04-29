b29d83ac3f3c5bc0d2cb5448ce8ba0fbe085e88e
Nicolas <148460157+Nicolab78@users.noreply.github.com>
2026-04-29 15:16:13 +0200

    Clean up imports in EmailService.java
    Removed unused imports related to ApplicationStatus, MessagingException, and MimeMessage.

--- recrutplus-backend/src/main/java/com/recrutplus/service/impl/EmailService.java
+++ recrutplus-backend/src/main/java/com/recrutplus/service/impl/EmailService.java
@@ -9,12 +9,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import com.recrutplus.model.enums.ApplicationStatus;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class EmailService {
